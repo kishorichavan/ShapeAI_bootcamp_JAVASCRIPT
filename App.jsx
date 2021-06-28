76e0c277c03eb9ae39ff3cb7fd777470ef79ab08
kishorichavan <86587284+kishorichavan@users.noreply.github.com>
2021-06-28 10:58:48 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import INFO from "./INFO";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <INFO />
+      <INFO />
+      <INFO />
+      <INFO />
+      <INFO />
+    </div>
+  );
+}
+
+export default App;
